9069e1ed34cd2f2324dc6469fd83552f2e27d0d5
Ruben F. Ruvalcaba <ruben_flores@discovery.com>
2023-04-21 19:26:24 -0400

    chore: App set up

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prettier'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'no-empty-function': 'off',
+    "no-var": "error",
+    "semi": "error",
+    "indent": "error",
+    "no-multi-spaces": "error",
+    "space-in-parens": "error",
+    "no-multiple-empty-lines": "error",
+    "prefer-const": "error",
+    "no-use-before-define": "error"
+  }
+}
